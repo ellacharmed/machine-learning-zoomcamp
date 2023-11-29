92384af5ea131d74835d30bc86f37b160e523d93
ellacharmed <ellacharmed@gmail.com>
2023-11-30 03:34:22 +0800

    Add localstack code for ecr testing

--- cohorts/2023/09-serverless/homework/lambda_function.py
+++ cohorts/2023/09-serverless/homework/lambda_function.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
 from io import BytesIO
 from urllib import request
 
 import numpy as np
 import tflite_runtime.interpreter as tflite
+# homework-08 uses Conv2D model, try to extend keras_image_helper ?
+# from keras_image_helper import create_preprocessor
 from PIL import Image
-
-# comment out for Qn 6
-MODEL_NAME = os.getenv('MODEL_NAME', 'bees-wasps-v2.tflite')
 
 
 def download_image(url):
@@ -27,41 +25,44 @@ def prepare_image(img, target_size):
     img = img.resize(target_size, Image.NEAREST)
     return img
 
-
+# replicate ImageDataGenerator(rescale=1./255)
 def preprocess_input(x):
     return x / 255.0
 
 
-interpreter = tflite.Interpreter(model_path=MODEL_NAME)
-interpreter.allocate_tensors()
 
-input_index = interpreter.get_input_details()[0]['index']
-output_index = interpreter.get_output_details()[0]['index']
-
-
-# 'https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg'
+target_size = (150, 150)
+classes = [
+    'bee',
+    'wasp'
+]
+# url = 'https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg'
 
 def predict(url):
     img = download_image(url)
-    img = prepare_image(img, target_size=(150, 150))
-
+    img = prepare_image(img, target_size)
     x = np.array(img, dtype='float32')
     X = np.array([x])
     X = preprocess_input(X)
 
+    interpreter = tflite.Interpreter(model_path='bees-wasps.tflite')
+    interpreter.allocate_tensors()
+
+    input_index = interpreter.get_input_details()[0]['index']
+    output_index = interpreter.get_output_details()[0]['index']
+
     interpreter.set_tensor(input_index, X)
     interpreter.invoke()
-
     preds = interpreter.get_tensor(output_index)
 
-    return float(preds[0, 0])
+    float_predictions = preds[0].tolist()
+
+    return dict(zip(classes, float_predictions))
 
 
 def lambda_handler(event, context):
     url = event['url']
-    pred = predict(url)
-    result = {
-        'prediction': pred
-    }
-
+    result = predict(url)
     return result
+
+
